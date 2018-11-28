911af97081f8ea762ce10adff6047a0540f2a974
kusumthakur <kltesting2018@gmail.com>
2018-11-28 23:11:33 +0000

    hi

---
+++ src/World.java
@@ -0,0 +1,11 @@
+public class World {
+    public static void main(String[]args){
+        int a = 35;
+        int b =45;
+        String x = "ram";
+        String y = "lakhan";
+        System.out.println(a>b);
+        System.out.println("world");
+        System.out.println(x+y);
+    }
+}
